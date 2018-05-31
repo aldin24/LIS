9ca604c124669f5f21bd063f06df7d59e5c15e48
KaricAnes <anes.karic@stu.ibu.edu.ba>
2018-06-01 00:25:45 +0200

    commit

--- application/views/librarian/editprofile.php
+++ application/views/librarian/editprofile.php
@@ -1,66 +1,72 @@
-<div class="panel panel-info">
-	<div class="panel-heading">
-		<h3 class="panel-title">Update Profile</h3>
-	</div>
-</div>
-<?= form_open_multipart('/librarian/editprofileaction') ?>
-<table class="table table-bordered">
-	<tr>
-		<td width="10%" rowspan="3" style="vertical-align:middle !important;">
-			<div class="fileinput fileinput-new" data-provides="fileinput">
-				<div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 200px;">
-					Click Here to Change Picture
-				</div>
-				<div>
-					<span class="btn btn-default btn-file fileinput-exists">
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Update Profile</h3>
+                    </div>
+                </div>
+                <form>
+                    <?= form_open_multipart('/librarian/editprofileaction') ?>
+                        <table class="table table-bordered">
+                            <tr>
+                                <td width="10%" rowspan="3" style="vertical-align:middle !important;">
+                                    <div class="fileinput fileinput-new" data-provides="fileinput">
+                                        <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 200px;">
+                                            Click Here to Change Picture
+                                        </div>
+                                        <div>
+                                            <span class="btn btn-default btn-file fileinput-exists">
 						<span class="fileinput-new">Select image</span>
-						<span class="fileinput-exists">Change</span>
-						<input type="file" name="dp" accept="image/jpeg,image/png,image/gif" />
-					</span>
-					<a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
-				</div>
-			</div>
-		</td>
-		<td width="90%" style="vertical-align:middle !important;">
-			<div class="form-group">
-				<label for="name">Full Name</label>
-				<input type="text" name="name" id="name" value="<?= $profile['full_name'] ?>"
-					class="form-control" required autofocus maxlength="200" />
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td style="vertical-align:middle !important;">
-			<div class="form-group">
-				<label for="email">E Mail</label>
-				<input type="email" name="email" id="email" value="<?= $profile['email'] ?>"
-					class="form-control" required maxlength="200" />
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td style="vertical-align:middle !important;">
-			<div class="form-group">
-				<label for="mobile">Mobile</label>
-				<input type="number" name="mobile" id="mobile" value="<?= $profile['phone'] ?>"
-					class="form-control" required max="999999999999999" />
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<div style="color:#f00 !important; font-size:14px !important; font-weight:bold !important;">
-				<?= validation_errors() ?>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2" align="center">
-			<button class="btn btn-success" style="width: 30%;">
+                                            <span class="fileinput-exists">Change</span>
+                                            <input type="file" name="dp" accept="image/jpeg,image/png,image/gif" />
+                                            </span>
+                                            <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td width="90%" style="vertical-align:middle !important;">
+                                    <div class="form-group">
+                                        <label for="name">Full Name</label>
+                                        <input type="text" name="name" id="name" value="<?= $profile['full_name'] ?>" class="form-control" required autofocus maxlength="200" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="vertical-align:middle !important;">
+                                    <div class="form-group">
+                                        <label for="email">E Mail</label>
+                                        <input type="email" name="email" id="email" value="<?= $profile['email'] ?>" class="form-control" required maxlength="200" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="vertical-align:middle !important;">
+                                    <div class="form-group">
+                                        <label for="mobile">Mobile</label>
+                                        <input type="number" name="mobile" id="mobile" value="<?= $profile['phone'] ?>" class="form-control" required max="999999999999999" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <div style="color:#f00 !important; font-size:14px !important; font-weight:bold !important;">
+                                        <?= validation_errors() ?>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" align="center">
+                                    <button class="btn btn-success" style="width: 30%;">
 				<span class="glyphicon glyphicon-export"></span>&nbsp;
 				Update Profile
 			</button>
-		</td>
-	</tr>
-</table>
-</form>
+                                </td>
+                            </tr>
+                        </table>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
--- application/views/librarian/viewbooks.php
+++ application/views/librarian/viewbooks.php
@@ -1,34 +1,54 @@
-<div class="panel panel-info">
-	<div class="panel-heading">
-		<h3 class="panel-title">List of <?= $toggle ?> Books</h3>
-	</div>
-</div>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
-<div class="">
-	<table class="table table-bordered table-hover" id="tableid">
-		<thead>
-		<tr>
-			<th>Book</th>
-			<th>Authors</th>
-			<th>Publisher</th>
-			<th>Shelf/Row</th>
-		</tr>
-		</thead>
-		<tbody>
-		<?php while( $book = $books->unbuffered_row() ): ?>
-			<tr>
-				<td><?= $book->title." - Ed. ".$book->edition." [".$book->copyid."]" ?></td>
-				<td><?= $authors[$book->id] ?></td>
-				<td><?= $book->pub ?></td>
-				<td><?= $book->shelf."/".$book->row ?></td>
-			</tr>
-		<?php endwhile ?>
-		</tbody>
-	</table>
-	<div style="height:50px;"> &nbsp; </div>
-</div>
-<script>
-$(document).ready(function(){
-	$('#tableid').DataTable();
-});
-</script>
+
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">List of
+                            <?= $toggle ?> Books</h3>
+                    </div>
+                </div>
+
+                <div class="">
+                    <table class="table table-bordered table-hover" id="tableid">
+                        <thead>
+                            <tr>
+                                <th>Book</th>
+                                <th>Authors</th>
+                                <th>Publisher</th>
+                                <th>Shelf/Row</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while( $book = $books->unbuffered_row() ): ?>
+                            <tr>
+                                <td>
+                                    <?= $book->title." - Ed. ".$book->edition." [".$book->copyid."]" ?>
+                                </td>
+                                <td>
+                                    <?= $authors[$book->id] ?>
+                                </td>
+                                <td>
+                                    <?= $book->pub ?>
+                                </td>
+                                <td>
+                                    <?= $book->shelf."/".$book->row ?>
+                                </td>
+                            </tr>
+                            <?php endwhile ?>
+                        </tbody>
+                    </table>
+                    <div style="height:50px;"> &nbsp; </div>
+                </div>
+                <script>
+                    $(document).ready(function() {
+                        $('#tableid').DataTable();
+                    });
+
+                </script>
+            </div>
+        </div>
+    </div>
+</section>
